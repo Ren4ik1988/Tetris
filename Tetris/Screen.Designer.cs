d0ea21bef4433a53a0ef604e6cea9b129644baba
Renzi <ren.zigan@gmail.com>
2017-08-11 11:32:28 +0300

    Коммит после удалению исключения из гитигнор

---
+++ Tetris/Screen.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Tetris
+{
+    partial class Screen
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Screen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Tetris.Properties.Resources.BackGraund;
+            this.DoubleBuffered = true;
+            this.Name = "Screen";
+            this.Size = new System.Drawing.Size(250, 500);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
