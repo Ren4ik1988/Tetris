f22fa95361bda3061156807fcbd7865ea9030a87
Renzi <ren.zigan@gmail.com>
2017-08-29 16:47:27 +0300

    Метод Run для каждого поворота фигуры "Конь" завершен

--- Tetris/Stallion1.cs
+++ Tetris/Stallion1.cs
@@ -76,14 +76,13 @@ namespace Tetris
         private void Random_0(ref int i, ref int j)
         {
             j = random.Next(0, 9);
-
             i2 = i + 2;
             j2 = j + 1;
             
             for (int k = i; k <= i2; k++)
             {
-                mainScreen[i, j].Image = Model.IsNotNull.Image;
-                onOff[i, j] = Model.On;
+                mainScreen[k, j].Image = Model.IsNotNull.Image;
+                onOff[k, j] = Model.On;
             }
 
             mainScreen[i2, j2].Image = Model.IsNotNull.Image;
@@ -107,18 +106,16 @@ namespace Tetris
 
         private bool Run_3(ref int i, ref int j)
         {
-            throw new NotImplementedException();
-        }
-
-        private bool Run_2(ref int i, ref int j)
-        {
             #region Первая часть метода: проверяет условие заполненности матрицы
-            j2 = j + 1;
-            i2 = i + 2;
-
             if (i2 == (Model.vertLength - 1) ||
-                    onOff[i2 + 1, j2] == Model.On )
+                    onOff[i2 + 1, j] == Model.On )
                 return false;
+
+            for (int k = j+1; k <= j2; k++)
+            {
+                if (onOff[i + 1, k] == Model.On)
+                    return false;
+            }
 
             #endregion
 
@@ -130,6 +127,9 @@ namespace Tetris
                 onOff[i, k] = Model.Off;
             }
 
+            mainScreen[i2, j].Image = Model.IsNull.Image;
+            onOff[i2, j] = Model.Off;
+
             #endregion
 
             #region Третья часть метода: отвечает за пермещение элементов массива на уровень ниже(шаг один квадрат)
@@ -137,23 +137,106 @@ namespace Tetris
             i++;
             i2++;
 
+            for (int k = j; k <= j2; k++)
+            {
+                mainScreen[i, k].Image = Model.IsNotNull.Image;
+                onOff[i, k] = Model.On;
+            }
+
+            mainScreen[i2, j].Image = Model.IsNotNull.Image;
+            onOff[i2, j] = Model.On;
+
+            return true;
+
+            #endregion
+        }
+
+        private bool Run_2(ref int i, ref int j)
+        {
+            #region Первая часть метода: проверяет условие заполненности матрицы
+
+            if (i2 == (Model.vertLength - 1) ||
+                    onOff[i2 + 1, j2] == Model.On ||
+                        onOff[i+1, j] == Model.On )
+                return false;
+
+            #endregion
+
+            #region Вторая часть метода, отвечает за очистку вверхних ячеек при перемещении объекта вниз
+
+            mainScreen[i, j].Image = Model.IsNull.Image;
+            onOff[i, j] = Model.Off;
+
+            for (int k = i; k <= i2; k++)
+            {
+                mainScreen[k, j2].Image = Model.IsNull.Image;
+                onOff[k, j2] = Model.Off;
+            }
+
+            #endregion
+
+            #region Третья часть метода: отвечает за пермещение элементов массива на уровень ниже(шаг один квадрат)
+
+            i++;
+            i2++;
+
+            mainScreen[i, j].Image = Model.IsNotNull.Image;
+            onOff[i, j] = Model.On;
+
             for (int k = i; k <= i2; k++)
             {
                 mainScreen[k, j2].Image = Model.IsNotNull.Image;
                 onOff[k, j2] = Model.On;
             }
-
-            mainScreen[i, j].Image = Model.IsNotNull.Image;
-            onOff[i, j] = Model.On;
             #endregion
 
             return true;
         }
-    
 
         private bool Run_1(ref int i, ref int j)
         {
-            throw new NotImplementedException();
+            #region Первая часть метода: проверяет условие заполненности матрицы
+            if (i2 == (Model.vertLength - 1) ) 
+                return false;
+
+            for(int k = j; k <= j2; k++)
+            {
+                if(onOff[i2+1, k] == Model.On)
+                    return false;
+            }
+
+
+            #endregion
+
+            #region Вторая часть метода, отвечает за очистку ячеек фигуры
+
+            for (int k = j; k <= j2; k++)
+            {
+                mainScreen[i2, k].Image = Model.IsNull.Image;
+                onOff[i2, k] = Model.Off;
+            }
+
+            mainScreen[i, j2].Image = Model.IsNull.Image;
+            onOff[i, j2] = Model.Off;
+
+            #endregion
+
+            #region Третья часть метода: отвечает за пермещение элементов массива на уровень ниже(шаг один квадрат)
+
+            i++;
+            i2++;
+
+            for (int k = j; k <= j2; k++)
+            {
+                mainScreen[i2, k].Image = Model.IsNotNull.Image;
+                onOff[i2, k] = Model.On;
+            }
+
+            mainScreen[i, j2].Image = Model.IsNotNull.Image;
+            onOff[i, j2] = Model.On;
+            #endregion
+
+            return true;
         }
 
         private bool Run_0(ref int i, ref int j)
@@ -163,20 +246,26 @@ namespace Tetris
                     onOff[i2+1,j] == Model.On ||
                         onOff[i2 +1, j2] == Model.On)
                 return false;
-            
+
             #endregion
 
             #region Вторая часть метода, отвечает за очистку вверхних ячеек при перемещении объекта вниз
 
-            mainScreen[i, j].Image = Model.IsNull.Image;
-            onOff[i, j] = Model.Off;
+            for (int k = i; k <= i2; k++)
+            {
+                mainScreen[k, j].Image = Model.IsNull.Image;
+                onOff[k, j] = Model.Off;
+            }
+
+            mainScreen[i2, j2].Image = Model.IsNull.Image;
+            onOff[i2, j2] = Model.Off;
 
             #endregion
 
             #region Третья часть метода: отвечает за пермещение элементов массива на уровень ниже(шаг один квадрат)
 
-            ++i;
-            i2 = i + 2;
+            i++;
+            i2++;
 
             for (int k = i; k <= i2; k++)
             {
